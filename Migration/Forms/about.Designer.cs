20212a7c89cc313a613b2838ad2131a6c6c86925
Dongjun Park <pdj5096@gmail.com>
2019-11-23 20:59:55 +0900

    Add files via upload

---
+++ Migration/Forms/about.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApp1.Forms
+{
+    partial class about
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Panel3 = new System.Windows.Forms.Panel();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.VerLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.PictureBox5 = new System.Windows.Forms.PictureBox();
+            this.PictureBox4 = new System.Windows.Forms.PictureBox();
+            this.PictureBox3 = new System.Windows.Forms.PictureBox();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseBT = new System.Windows.Forms.PictureBox();
+            this.Panel3.SuspendLayout();
+            this.Panel1.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RichTextBox1.Location = new System.Drawing.Point(10, 40);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.ReadOnly = true;
+            this.RichTextBox1.Size = new System.Drawing.Size(499, 112);
+            this.RichTextBox1.TabIndex = 0;
+            this.RichTextBox1.Text = "";
+            // 
+            // Panel3
+            // 
+            this.Panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Panel3.Controls.Add(this.RichTextBox1);
+            this.Panel3.Controls.Add(this.Panel1);
+            this.Panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Panel3.Location = new System.Drawing.Point(0, 125);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Padding = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.Panel3.Size = new System.Drawing.Size(519, 162);
+            this.Panel3.TabIndex = 10;
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.PictureBox5);
+            this.Panel1.Controls.Add(this.Label1);
+            this.Panel1.Controls.Add(this.PictureBox4);
+            this.Panel1.Controls.Add(this.PictureBox3);
+            this.Panel1.Controls.Add(this.PictureBox2);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel1.Location = new System.Drawing.Point(10, 0);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(499, 40);
+            this.Panel1.TabIndex = 6;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("나눔스퀘어", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Label1.Location = new System.Drawing.Point(3, 16);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(272, 18);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "몬지 (monZi) - 더 나은 공기, 더 나은 삶";
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(79)))), ((int)(((byte)(61)))));
+            this.TopPanel.Controls.Add(this.VerLabel);
+            this.TopPanel.Controls.Add(this.TitleLabel);
+            this.TopPanel.Controls.Add(this.CloseBT);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.TopPanel.Size = new System.Drawing.Size(519, 30);
+            this.TopPanel.TabIndex = 11;
+            // 
+            // VerLabel
+            // 
+            this.VerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.VerLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.VerLabel.ForeColor = System.Drawing.Color.White;
+            this.VerLabel.Location = new System.Drawing.Point(363, 0);
+            this.VerLabel.Name = "VerLabel";
+            this.VerLabel.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.VerLabel.Size = new System.Drawing.Size(109, 30);
+            this.VerLabel.TabIndex = 9;
+            this.VerLabel.Text = "VerLabel";
+            this.VerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TitleLabel.Font = new System.Drawing.Font("나눔스퀘어", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TitleLabel.ForeColor = System.Drawing.Color.White;
+            this.TitleLabel.Location = new System.Drawing.Point(10, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(139, 30);
+            this.TitleLabel.TabIndex = 4;
+            this.TitleLabel.Text = "monZi 정보";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PictureBox5
+            // 
+            this.PictureBox5.Image = global::WindowsFormsApp1.Properties.Resources.info_logo;
+            this.PictureBox5.Location = new System.Drawing.Point(400, 6);
+            this.PictureBox5.Name = "PictureBox5";
+            this.PictureBox5.Size = new System.Drawing.Size(28, 28);
+            this.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox5.TabIndex = 5;
+            this.PictureBox5.TabStop = false;
+            // 
+            // PictureBox4
+            // 
+            this.PictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.info_gmail;
+            this.PictureBox4.Location = new System.Drawing.Point(468, 6);
+            this.PictureBox4.Name = "PictureBox4";
+            this.PictureBox4.Size = new System.Drawing.Size(28, 28);
+            this.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox4.TabIndex = 4;
+            this.PictureBox4.TabStop = false;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.info_tg;
+            this.PictureBox3.Location = new System.Drawing.Point(366, 6);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(28, 28);
+            this.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox3.TabIndex = 3;
+            this.PictureBox3.TabStop = false;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.info_blog;
+            this.PictureBox2.Location = new System.Drawing.Point(434, 6);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(28, 28);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 2;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.BackColor = System.Drawing.Color.White;
+            this.PictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.infoban;
+            this.PictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(519, 95);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 9;
+            this.PictureBox1.TabStop = false;
+            // 
+            // CloseBT
+            // 
+            this.CloseBT.BackColor = System.Drawing.Color.Transparent;
+            this.CloseBT.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseBT.Image = global::WindowsFormsApp1.Properties.Resources.closeicon;
+            this.CloseBT.Location = new System.Drawing.Point(472, 0);
+            this.CloseBT.Name = "CloseBT";
+            this.CloseBT.Size = new System.Drawing.Size(47, 30);
+            this.CloseBT.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CloseBT.TabIndex = 3;
+            this.CloseBT.TabStop = false;
+            // 
+            // about
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(519, 287);
+            this.Controls.Add(this.Panel3);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.TopPanel);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "about";
+            this.Text = "about";
+            this.Panel3.ResumeLayout(false);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBT)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.RichTextBox RichTextBox1;
+        internal System.Windows.Forms.Panel Panel3;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.PictureBox PictureBox5;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.PictureBox PictureBox4;
+        internal System.Windows.Forms.PictureBox PictureBox3;
+        internal System.Windows.Forms.PictureBox PictureBox2;
+        internal System.Windows.Forms.Panel TopPanel;
+        internal System.Windows.Forms.Label VerLabel;
+        internal System.Windows.Forms.Label TitleLabel;
+        internal System.Windows.Forms.PictureBox CloseBT;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+    }
+}
---
+++ Migration/Forms/about.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Forms
+{
+    public partial class about : Form
+    {
+        public about()
+        {
+            InitializeComponent();
+        }
+
+        private void FadeInEffect(object sender, EventArgs e)
+        {
+            this.Refresh();
+            FadeIn(this, 1);
+        }
+
+        private void FadeOutEffect(object sender, EventArgs e)
+        {
+            FadeOut(this);
+            MainGUI.Show();
+            FadeIn(MainGUI, 1);
+        }
+
+        private void FormDrag_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                loc = e.Location;
+        }
+
+        private void FormDrag_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                this.Location += e.Location - loc;
+        }
+
+        private void about_Load(object sender, EventArgs e)
+        {
+            VerLabel.Text = My.Application.Info.Version.ToString;
+            RichTextBox1.Text = My.Resources.appinfo;
+            Opacity = 0;
+            if (My.Settings.UseNativeFont)
+                ChangeToNativeFont(this);
+        }
+
+        private void CloseBT_MouseEnter(object sender, EventArgs e)
+        {
+            CloseBT.BackColor = ControlPaint.Light(TopPanel.BackColor, 0.2);
+        }
+
+        private void CloseBT_MouseLeave(object sender, EventArgs e)
+        {
+            CloseBT.BackColor = Color.Transparent;
+        }
+
+        private void CloseBT_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // 지메일
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            Process.Start("mailto:pdj5096@gmail.com");
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://t.me/pbjun");
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://pbjsw.kro.kr");
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://blog.pbj.kr");
+        }
+
+    }
+}
